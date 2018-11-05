4b8786a6fc1928f61e848d5051e936b8ea3d2bc2
Loiane <loianeg@gmail.com>
2018-11-05 09:04:49 -0500

    oracle cloud hello world

--- spring-cloud-hello/src/main/java/com/loiane/springcloudhello/repository/ContactRepository.java
+++ spring-cloud-hello/src/main/java/com/loiane/springcloudhello/repository/ContactRepository.java
@@ -2,6 +2,8 @@ package com.loiane.springcloudhello.repository;
 
 import com.loiane.springcloudhello.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
